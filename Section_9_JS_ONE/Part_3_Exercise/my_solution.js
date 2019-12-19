12917909e3641796e87db2e36ef414705ac4426f
Mike F <michael.f.furtado@gmail.com>
2019-12-19 14:44:39 -0500

    exercise 3 solution code

--- Section_9_JS_ONE/Part_3_Exercise/my_solution.js
+++ Section_9_JS_ONE/Part_3_Exercise/my_solution.js
@@ -1,1 +1,6 @@
 // my solution for javascript one part 3 Exercise
+alert("Welcome to weight convesion progam");
+var pounds = prompt("Enter a weight in pound to covert to kilograms");
+var kilos = pounds * 0.454;
+alert(pounds + " pounds is " + kilos + " kilograms");
+console.log("Conversion Completed");
