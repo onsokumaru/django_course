21448397cd85f445614c1fb497a4a4a1d4687ef4
onsokumaru <11082686+onsokumaru@users.noreply.github.com>
2019-12-18 20:44:29 -0500

    added lecture code

---
+++ Section_9_JS_ONE/Part_2/myscript.js
@@ -0,0 +1,5 @@
+alert("Welcome to your Bank!");
+
+var deposit = prompt("How much would you like to deposit today?");
+alert("You've deposited: " + deposit + " dollars");
+console.log("You are a cool person!");
