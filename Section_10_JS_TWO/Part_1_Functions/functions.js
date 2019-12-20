79b26659a0edf54f27cb57b7c402821e525d169a
Mike F <michael.f.furtado@gmail.com>
2019-12-20 11:43:07 -0500

    added code from functions lecture

---
+++ Section_10_JS_TWO/Part_1_Functions/functions.js
@@ -0,0 +1,21 @@
+// Functions
+function hello(){
+  console.log("Hello");
+}
+
+function helloYou(name){
+  console.log("Hello " + name);
+}
+
+function addNum(num1, num2) {
+  console.log(num1 + num2);
+}
+
+function helloSomeone(name="Rachel") {
+  console.log("Hello " + name);
+}
+
+function formal(name="Mike", title="MC"){
+  // console.log(title + " " + name);
+  return title + " " + name;
+}
