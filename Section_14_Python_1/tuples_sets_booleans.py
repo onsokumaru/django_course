d0862eeddf2e37e5f64b4c2cd526ae2649fb3c22
Mike F <michael.f.furtado@gmail.com>
2020-01-07 15:23:35 -0500

    added relevant code from lecture

--- Section_14_Python_1/tuples_sets_booleans.py
+++ Section_14_Python_1/tuples_sets_booleans.py
@@ -1,1 +1,32 @@
 # tuples, sets, and booleans
+
+#tuples are immutable
+
+# booleans
+True
+False
+0
+1
+
+# tuples
+t = (1,2,3)
+print(t[1])
+
+u = ('a', True, 123.4)
+print(u)
+
+# sets are unordered collections of unique elements
+x = set()
+x.add(1)
+x.add(2)
+print(x)
+x.add(4)
+x.add(0.1)
+print(x)
+x.add(4) # will only accept unique elements
+x.add(4)
+x.add(4)
+print(x)
+
+converted = set([1,1,1,1,1,2,2,2,2,2,3,3,3,3,3])
+print(converted)
