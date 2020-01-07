249529bdefb375a8b4b2d65ee2445efb5f5ccbb0
Mike F <michael.f.furtado@gmail.com>
2020-01-07 10:54:10 -0500

    added code from the lecture

--- Section_14_Python_1/strings.py
+++ Section_14_Python_1/strings.py
@@ -1,10 +1,40 @@
 # STRINGS
 
+#INDEXING
 mystring = 'abcdefg'
 print(mystring[-1])
 
-#INDEXING
-
 #SLICING
 
+print(mystring[4:])
+
+print(mystring[:3])
+
+print(mystring[2:5])
+
+print(mystring[::2])
+
 #Basic Methods
+x = mystring.upper()
+print(x)
+
+x = mystring.lower()
+print(x)
+
+x = mystring.capitalize()
+print(x)
+
+mystring = "Hello World"
+x = mystring.split()
+print(x)
+
+#print formatting
+print("Insert another string here: {}".format("INSERT ME"))
+
+print("Item One: {} Item Two: {}".format("dog", "cat"))
+
+print("Item One: {n} Item Two: {m}".format(m = "dog", n = "cat"))
+
+a = "first insert"
+b = "second insert"
+print (f"Insert two strings, string 1: {a} and string 2: {b}")
